0f889dcd69c68ac44510406587c2b7392a939975
alixiasenining <s3825167@student.rmit.edu.au>
2020-10-12 17:30:57 +1100

    This is alixia testing Github

--- app/src/main/java/com/ulan/timetable/activities/TestCommit.java
+++ app/src/main/java/com/ulan/timetable/activities/TestCommit.java
@@ -5,4 +5,5 @@ public class TestCommit {
     String test = "This is to test the commit";
     String test2 = "I am testing";
     String test3 = "This is alixia testing Github";
+    String test4 = "This is alixia testing again";
 }
