ab3e050fe44a8f3db3f2a81fe18f2aa4e1cb4237
LiesVDS <liesjevandensteen@hotmail.com>
2020-10-15 14:38:10 +1100

    OnBoarding Screen for branche - crashing app

---
+++ app/src/main/java/com/ulan/timetable/activities/OnBoardingActivity.java
@@ -0,0 +1,107 @@
+package com.ulan.timetable.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.ulan.timetable.R;
+import com.ulan.timetable.utils.SliderAdapter;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    //Variables
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+    SliderAdapter sliderAdapter;
+    TextView[] dots;
+    Button letsGetStarted;
+    int currentPos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_on_boarding);
+
+        //Hooks
+        viewPager = findViewById(R.id.slider);
+        dotsLayout = findViewById(R.id.dots);
+        letsGetStarted = findViewById(R.id.get_started_btn);
+
+        //Call adapter
+        sliderAdapter = new SliderAdapter(this);
+        viewPager.setAdapter(sliderAdapter);
+
+        //Dots
+        addDots(0);
+        viewPager.addOnPageChangeListener(changeListener);
+    }
+
+    public void skip(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    public void next(View view) {
+        viewPager.setCurrentItem(currentPos + 1);
+    }
+
+    private void addDots(int position) {
+
+        dots = new TextView[5];
+        dotsLayout.removeAllViews();
+
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("•"));
+            dots[i].setTextSize(35);
+
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0) {
+            dots[position].setTextColor(getResources().getColor(R.color.colorPurple));
+        }
+
+    }
+
+    ViewPager.OnPageChangeListener changeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDots(position);
+            currentPos = position;
+
+            //set button invisible until reach of last page
+            if (position == 0) {
+                letsGetStarted.setVisibility(View.INVISIBLE);
+            } else if (position == 1) {
+                letsGetStarted.setVisibility(View.INVISIBLE);
+            } else if (position == 2) {
+                letsGetStarted.setVisibility(View.INVISIBLE);
+            } else if (position == 3) {
+                letsGetStarted.setVisibility(View.INVISIBLE);
+            }else {
+                letsGetStarted.setVisibility(View.VISIBLE);
+            }
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+}
